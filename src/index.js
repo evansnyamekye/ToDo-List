030b003b2db669e798358eaee89799d0ff51d230
evansnyamekye <60971417+evansnyamekye@users.noreply.github.com>
2023-08-18 13:39:46 +0000

    Add working directories

---
+++ src/index.js
@@ -0,0 +1,34 @@
+// import _ from 'lodash';
+// import './style.css';
+
+// function component() {
+//     const element = document.createElement('div');
+  
+    // Lodash, currently included via a script, is required for this line to work
+      // Lodash, now imported by this script
+//     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+//     element.classList.add('hello');
+  
+//     return element;
+//   }
+  
+//   document.body.appendChild(component());
+import './style.css';
+import todoData from './modules/data';
+
+const ul = document.querySelector('.ul');
+
+function displayTask() {
+  todoData.forEach((data) => {
+    const list = document.createElement('li');
+    list.innerHTML = `
+      <div><input type="checkbox" id="check">
+        <p>${data.description}</p>
+      </div>
+      <i class="fas fa-ellipsis-v"></i>
+    `;
+    ul.appendChild(list);
+  });
+}
+
+document.addEventListener('DOMContentLoaded', displayTask);
---
+++ src/modules/data.js
@@ -0,0 +1,14 @@
+const todoData = [
+    {
+      index: 1,
+      description: 'wash the dishes',
+      completed: false,
+    },
+    {
+      index: '01',
+      description: 'complete todo list project',
+      completed: false,
+    },
+  ];
+  
+  export default todoData;
